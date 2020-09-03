5ec7a0ba3bbca839ced292c23e2a9f4135ea2963
jtdevuk <44753684+jtdevuk@users.noreply.github.com>
2020-09-03 12:26:48 +0100

    Complete bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(array)
+
+  def sort(array, num)
+    array[num], array[num+1] = array[num+1], array[num]
+  end
+  
+  (array.length-1).times do
+    array.each_index do |num|        
+        if num < array.length-1 
+          array[num] > array[num + 1] ? sort(array, num) : next          
+        end
+      end     
+    end 
+  
+  print array
+end
+
+bubble_sort([4,3,78,2,0,2])
